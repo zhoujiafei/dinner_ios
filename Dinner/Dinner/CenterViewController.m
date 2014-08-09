123d79123a2c03092468908d1204af11afdba59f
zhoujiafei <liujinlan@liumatoMacBook-Air.local>
2014-08-09 22:24:41 +0800

    更换头像异步
    更换头像异步

--- Dinner/Dinner/CenterViewController.m
+++ Dinner/Dinner/CenterViewController.m
@@ -89,7 +89,10 @@
                                     [[_userInfo objectForKey:@"avatar"] objectForKey:@"filepath"],
                                     [[_userInfo objectForKey:@"avatar"] objectForKey:@"filename"]];
             
-            [_pathCover setAvatarImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:avatarPath]]]];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [_pathCover setAvatarImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:avatarPath]]]];
+            });
+            
         }
         else
         {
@@ -304,7 +307,10 @@
                                         [[_userInfo objectForKey:@"avatar"] objectForKey:@"filepath"],
                                         [[_userInfo objectForKey:@"avatar"] objectForKey:@"filename"]];
                 
-                [_pathCover setAvatarImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:avatarPath]]]];
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    [_pathCover setAvatarImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:avatarPath]]]];
+                });
+                
             }
             else
             {
